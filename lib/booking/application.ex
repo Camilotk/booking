1be2d1c995da7bc09b01b1aa7e8b65f9b9c0ea8a
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-08-22 23:33:13 -0300

    first commit

---
+++ lib/booking/application.ex
@@ -0,0 +1,20 @@
+defmodule Booking.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Booking.Worker.start_link(arg)
+      # {Booking.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Booking.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
